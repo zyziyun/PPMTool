c713676a36245b4376baaf971fb80c3568269ae1
Yun Zi <yzi1@hawk.iit.edu>
2022-08-15 11:36:25 -0500

    feature: add redux & axios

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
+import { Provider } from "react-redux";
+import store from "./store";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
+            <Routes>
+              <Route exact path="/dashboard" element={<Dashboard />}  />
+              <Route exact path="/addProject" element={<AddProject />} />
+            </Routes>
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
